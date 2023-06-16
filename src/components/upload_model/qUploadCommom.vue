d4d7cccdd0879195bb3642de2f49e8810517d8e3
Viet Le <viet2000yyy@gmai.com>
2023-06-16 18:09:21 +0700

    upload_file_part_1

--- src/components/upload_model/qUploadCommom.vue
+++ src/components/upload_model/qUploadCommom.vue
@@ -4,16 +4,28 @@
       <q-form @submit="onSubmit">
         <div class="row justify-center">
           <q-uploader
+            ref="uploader"
             label="Images / Videos / Files"
             square
             flat
             multiple
+            removeUploadedFiles
             @added="onFileAdded"
+            @removed="removeFlies"
             bordered
           />
         </div>
         <div class="row justify-center">
-          <q-btn label="Add" type="submit" color="primary" class="q-ma-sm" />
+          <q-btn
+            :disable="filesList.length < 1 ? true : false"
+            class="q-ma-sm"
+            type="submit"
+            dark-percentage
+            color="primary"
+            text-color="grey-9"
+            icon="cloud_upload"
+            style="width: 100px"
+          />
         </div>
       </q-form>
     </q-card>
@@ -24,15 +36,43 @@
 import { storage } from '@/firebase'
 import { ref as storageRef, uploadBytes } from 'firebase/storage'
 import { ref } from 'vue'
+import { userLoginStore } from '@/stores/user.js'
+import { useLoadingStore } from '@/stores/loading'
 
-const filesList = ref(null)
+const filesList = ref([])
+const userStore = userLoginStore()
+const isLoadingStore = useLoadingStore()
+const uploader = ref(null)
 
 const onFileAdded = (files) => {
   filesList.value = [...files]
 }
 
-const onSubmit = () => {
-  console.log('123')
+const removeFlies = (files) => {
+  console.log(files)
+}
+
+const onSubmit = async () => {
+  const folder = `${userStore.name}_${userStore.id}`
+
+  if (!filesList.value) {
+    console.log('No value')
+    return
+  } else {
+    isLoadingStore.setLoading(false)
+    await Promise.all(
+      filesList.value.map(async (file) => {
+        const stRef = storageRef(storage, `${folder}/` + file.name)
+        await uploadBytes(stRef, file).then((snapshot) => {
+          console.log('success')
+          // Remove the uploaded file from filesList
+          filesList.value = filesList.value.filter((f) => f !== file)
+        })
+      })
+    )
+    uploader.value.reset()
+    isLoadingStore.setLoading(false)
+  }
 }
 </script>
 
